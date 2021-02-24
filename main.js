2c616ae256848dd9924214d2616d0cdcad470d96
cvillar22 <csorrochevillar@gmail.com>
2021-02-24 17:18:21 -0300

    modificaciones guille

--- main.js
+++ main.js
@@ -1,126 +1,218 @@
 /*
-Grilla del abecedario => array
-palabra que juega => comand promt
-el muñeco que ahorca= 6 intentos => variable numerica
-
 Acciones:
+1. Usuario hace click en una letra determinada.
+2. Esa letra queda deshabilitada y se filtra del abecedario.
+3. Se chequea que esté la letra y en qué posición de la palabra.
+4. Se chequea que la palabra esté completada, si es así el usuario gana.
+5. El usuario puede arriesgar por medio de un input para tratar de adivinar.
+6. Si el usuario no tiene mas vidas, pierde la partida.
 
-ingresar una letra,
-comparar lo que ingreso con lo que está.
-aparecer en pantalla si esta
-y tacharla del abecedario
-y quitar intentos.
-fin de juego.
-
-mostrar la palabra magic completa
 */
-let palabras = ["Sonrojarse" , "Llaves", "Escuadra", "Obsequio" , "Cera", "Bicicleta", 
-"Resbalar", "Virus", "telescopio", "dinosaurio"];
 
-let palabra;
+const player = {
+    tries: 6,
+    canPlay: true,
 
-let wordMagic;
+    continuePlaying() {
+        this.canPlay = this.tries > 0;
+        return this.canPlay;
+    },
 
-let magicArray;
+    decrementTries() {
+        this.tries = this.tries - 1;
+    },
 
-let tries = 6;
+    getTries() {
+        return this.tries;
+    },
 
-let abecedario = ["A","B","C","D", "E", "F", "G",
-"H", "I", "J", "K","L", "M", "N", "Ñ", "O", "P", "Q", "R",
-"S", "T", "U", "V","W", "X", "Y", "Z"];
-
-let containerAbc = document.getElementById("abc");
-
-let vidas = document.getElementById("vidas");
-
-let texto = document.getElementById("texto");
-
-let arriesgar = document.getElementById("arriesgar");
-
-let input = document.getElementById("input");
-
-containerAbc.addEventListener("click",botoncinio);
-
-arriesgar.addEventListener("click", arriesgazao);
-
-renderazaoABC(abecedario);
-
-pandoraSetUp(palabras);
-
-renderJuego();
-
-function laComparazao(letra, wordMagic){
-	let hasWord = wordMagic.some(word => word == letra);
-	if(hasWord){
-		for(let word in wordMagic){
-			if(letra == wordMagic[word]){
-				magicArray[word] = letra;
-			}
-		}
-	}else{
-		tries = tries - 1;
-	};
-	abecedario = abecedario.filter(caracter => caracter != letra);
+    updatePlayer(boolean) {
+        this.canPlay = boolean;
+    },
 };
 
-function playground(letra){
-	if(tries > 0){
-		laComparazao(letra, wordMagic);
-		let magic = wordMagic.join(" ");
-		let word = magicArray.join(" ");
-		if(magic == word){
-		alert("GANASTE PUTO");
-		}
-	}else{
-		alert("PUTO INVERBE MANCO");
-	}
-	
+const words = [
+    "Sonrojarse",
+    "Llaves",
+    "Escuadra",
+    "Obsequio",
+    "Cera",
+    "Bicicleta",
+    "Resbalar",
+    "Virus",
+    "Telescopio",
+    "Dinosaurio",
+];
+
+const alphabet = {
+    letters: [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "Ñ",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ],
+
+    filterLetter(letter) {
+        this.letters = this.letters.filter((character) => character != letter);
+    },
 };
 
-function renderazaoABC(abecedario){
+const selectedWord = getRandomWord(words);
+const wordArray = createArrayWord(selectedWord);
+const gameArray = createEmptyArray(selectedWord);
 
-	for(let letra of abecedario){
-		let button = document.createElement("button");
-		button.value = letra;
-		button.textContent = letra;
-		button.classList.add("btn","btn-info");
-		containerAbc.appendChild(button);
-	}
-};
+/* HTML ELEMENTS */
+const htmlAlphabet = document.getElementById("abc");
+const htmlLifes = document.getElementById("vidas");
+const htmlWord = document.getElementById("texto");
+const htmlGuest = document.getElementById("arriesgar");
 
-	function botoncinio(evento){
+renderAlphabet(alphabet.letters);
+renderTries(player.tries);
+renderGameWord(gameArray);
 
-		if(evento.target.classList.contains("btn-info")){
-			evento.target.disabled = true;
-			playground(evento.target.value);
-			renderJuego();
-		};
+htmlAlphabet.addEventListener("click", guestLetter);
+htmlAlphabet.addEventListener("click", winByClick);
+htmlGuest.addEventListener("click", winByInput);
 
-	}
+/* SETUP FUNCTIONS */
 
-	function renderJuego(){
-		vidas.textContent = tries;
-		texto.textContent = magicArray.join(' ');
-	};
+function getRandomWord(words) {
+    const index = Math.floor(Math.random() * (words.length - 1));
+    return words[index].toUpperCase();
+}
 
-	function pandoraSetUp(palabras){
-		let random = Math.floor(Math.random()*palabras.length);
-		console.log(random);
-		palabra = palabras[random].toUpperCase();
-		wordMagic = palabra.split("");
-		magicArray = '_'.repeat(palabra.length).split('');
-	};
+function createArrayWord(word) {
+    return word.split("");
+}
 
-	function arriesgazao(evento){
-		let arriesgada = input.value.toUpperCase();
-		if (arriesgada != ""){
-			if(arriesgada == palabra){
-				alert("GANASTE PUTO");
+function createEmptyArray(word) {
+    return "_".repeat(word.length).split("");
+}
 
-			}else{
-				tries = tries - 1;
-			};
-		}else{
-			tries = tries - 1;
-		}
-	};
+/* RENDER HTML FUNCIONS */
+
+function renderAlphabet(alphabet) {
+    for (let letter of alphabet) {
+        let button = document.createElement("button");
+        button.value = letter;
+        button.textContent = letter;
+        button.classList.add("btn", "btn-info");
+        htmlAlphabet.appendChild(button);
+    }
+}
+
+function renderTries(tries) {
+    htmlLifes.textContent = tries;
+}
+
+function renderGameWord(gameArray) {
+    htmlWord.textContent = gameArray.join(" ");
+}
+
+/* GAME FUNCIONS */
+
+function checkLetter(letter) {
+    const hasWord = wordArray.some((character) => character === letter);
+
+    if (hasWord) {
+        updateGameArray(letter);
+    } else {
+        player.decrementTries();
+    }
+
+    alphabet.filterLetter(letter);
+}
+
+function updateGameArray(letter) {
+    for (let word in wordArray) {
+        if (letter == wordArray[word]) {
+            gameArray[word] = letter;
+        }
+    }
+}
+
+function guestWord(word, selectedWord) {
+    return word == selectedWord;
+}
+
+function checkGameStatus(hasWin, canPlay) {
+    if (hasWin) {
+        successLog("Has ganado!");
+        player.canPlay = false;
+    } else if (!canPlay) {
+        errorLog(`Has Perdido! La palabra correcta era: ${selectedWord}`);
+    }
+}
+
+/* GAME EVENTS */
+
+function guestLetter(event) {
+    if (event.target.classList.contains("btn-info") && player.canPlay) {
+        event.target.disabled = true;
+        checkLetter(event.target.value, wordArray, gameArray);
+        renderGameWord(gameArray);
+        renderTries(player.tries);
+    }
+}
+
+function winByClick() {
+    if (player.canPlay) {
+        const gameWord = gameArray.join("");
+        const hasWin = guestWord(gameWord, selectedWord);
+        checkGameStatus(hasWin, player.continuePlaying());
+    }
+}
+
+function winByInput() {
+    if (player.canPlay) {
+        const userGuest = document.getElementById("input").value.toUpperCase();
+        const hasWin = guestWord(userGuest, selectedWord);
+
+        if (!hasWin) {
+            player.decrementTries();
+            renderTries(player.tries);
+        }
+
+        checkGameStatus(hasWin, player.continuePlaying());
+    }
+}
+
+/* MESSAGES */
+
+function successLog(msg) {
+    console.log(
+        `%c${msg}`,
+        "background-color: #00ff00 ; color: #00; padding: 4px; font-weight: bold;"
+    );
+}
+
+function errorLog(msg) {
+    console.log(
+        `%c${msg}`,
+        "background-color: #FF9494 ; color: #ff; padding: 4px; font-weight: bold;"
+    );
+}
